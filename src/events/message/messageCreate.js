87b1c1157ca70fdba4a468f78eade498cb155da1
MannuVilasara <mannuvilasara@gmail.com>
2024-05-07 19:33:21 +0530

    feat: ✨

--- src/events/message/messageCreate.js
+++ src/events/message/messageCreate.js
@@ -1,7 +1,7 @@
 const { commandHandler, automodHandler, statsHandler } = require("@src/handlers");
 const { PREFIX_COMMANDS } = require("@root/config");
 const { getSettings } = require("@schemas/Guild");
-const { EmbedBuilder } = require('discord.js')
+// const { EmbedBuilder } = require('discord.js')
 
 /**
  * @param {import('@src/structures').BotClient} client
@@ -17,29 +17,29 @@ module.exports = async (client, message) => {
     // check for bot mentions
     if (message.content.includes(`${client.user.id}`)) {
       const embed = {
-          "title": "Did someone mention me? <:shadow_what:1236958475200757831>",
-          "description": "<:shadow_happy:1236957987780956211> Hello there! My prefix is `s$`",
-          "color": 0x068add, 
-          "timestamp": new Date(),
-          "thumbnail": {
-            "url": message.client.user.avatarURL({ format: "png" })
+        "title": "Did someone mention me? <:shadow_what:1236958475200757831>",
+        "description": "<:shadow_happy:1236957987780956211> Hello there! My prefix is `s$`",
+        "color": 0x068add,
+        "timestamp": new Date(),
+        "thumbnail": {
+          "url": message.client.user.avatarURL({ format: "png" })
         },
-          "fields": [
-            {
-               "name": "Feel lost?",
-               "value": "To view my commands list, you can do `s$help` or `/help` <:shadow_working:1236958444754440314>",
-               "inline": false
-           },
-           {
-               "name": "",
-               "value": "",
-               "inline": false
-           },
-          ],
+        "fields": [
+          {
+            "name": "Feel lost?",
+            "value": "To view my commands list, you can do `s$help` or `/help` <:shadow_working:1236958444754440314>",
+            "inline": false
+          },
+          {
+            "name": "",
+            "value": "",
+            "inline": false
+          },
+        ],
       };
-  
-      message.channel.safeSend({embeds: [embed]})
-  }
+
+      message.channel.safeSend({ embeds: [embed] })
+    }
 
     if (message.content && message.content.startsWith(settings.prefix)) {
       const invoke = message.content.replace(`${settings.prefix}`, "").split(/\s+/)[0];
